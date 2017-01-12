8ddc0f00169bff773374abc4d82ea03ac0b01a24
Elon <xamantechmail@gmail.com>
2017-01-12 00:42:58 -0500

    add xt-mvc

---
+++ js/xt-mvc.js
@@ -0,0 +1,34 @@
+
+$(function(){
+   /* ======= Model ======= */
+
+    var model = {
+      
+    };
+
+/* ======= Octopus ======= */
+
+    var octopus = {
+        init: function() {
+
+
+        }
+
+    };
+
+/* ======= View ======= */
+
+    var view = {
+        init: function() {
+
+            view.render();
+        },
+
+        render: function(){
+
+        }
+    };
+
+    octopus.init();
+
+});
